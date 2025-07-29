754780f4591fc4df00d12941a3fae85d4bc9195a
vidhi-131 <vidhi.ratan.btech2024@sitpune.edu.in>
2025-07-30 01:45:11 +0530

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,33 @@
+//VIDHI RATAN
+//24070123131
+//B2
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1,num2,sum=0,sub=0,mul=0,divi=0;
+    cout <<"Enter first number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    sum=num1+num2;
+    cout<<" Sum of given nos. is: "<<sum<<endl;
+    sub=num1-num2;
+    cout<<" Subtration of given nos. is: "<<sub;
+    mul=num1*num2;
+    cout<<"\n Multiplication of given nos. is: "<<mul;
+    divi=num1/num2;
+    cout<<"\n Division of given nos. is: "<<divi;
+    return 0;
+}
+
+/*
+output:
+Enter first number: 7
+Enter second number: 8
+Sum of given nos. is: 15
+Subtration of given nos. is: -1
+Multiplication of given nos. is: 56
+Division of given nos. is: 0
+*/
