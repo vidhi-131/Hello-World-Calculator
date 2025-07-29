fefc9527f408dab9df4ac1dd220adc4dc4da41ea
vidhi-131 <vidhi.ratan.btech2024@sitpune.edu.in>
2025-07-30 01:47:31 +0530

    Create Helloworld.cpp

---
+++ Helloworld.cpp
@@ -0,0 +1,18 @@
+//VIDHI RATAN
+//24070123131
+//B2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Hello World!";
+
+    return 0;
+}
+
+/*
+output:
+Hello World!
+*/
